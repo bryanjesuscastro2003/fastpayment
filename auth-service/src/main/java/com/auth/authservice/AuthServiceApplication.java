94df25b32505ba367d47ae587a62c90412add0b2
bryanJesus2003 <jesusbryan155@gmail.com>
2023-04-22 16:44:13 -0600

    630

--- auth-service/src/main/java/com/auth/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/auth/authservice/AuthServiceApplication.java
@@ -3,12 +3,12 @@ package com.auth.authservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
+@EnableFeignClients
 public class AuthServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServiceApplication.class, args);
 	}
-
 }
