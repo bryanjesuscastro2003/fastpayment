5a2df01d04d702533f3d99a0d64feff897ddd00c
bryanJesus2003 <jesusbryan155@gmail.com>
2023-04-21 18:14:10 -0600

    614

--- route-config/src/main/java/com/route/config/RouteConfigApplication.java
+++ route-config/src/main/java/com/route/config/RouteConfigApplication.java
@@ -2,8 +2,10 @@ package com.route.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class RouteConfigApplication {
 
 	public static void main(String[] args) {
